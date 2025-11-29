14b6b58e78818c2e53e5f3e8d815dd5d71986538
Rahmadini <rahmadini3211@student.unri.ac.id>
2025-11-29 15:17:21 +0700

    install breeze

--- resources/views/product/list.blade.php
+++
@@ -1,9 +0,0 @@
-<h1>Halaman Product</h1>
-<h2>Category : {{ $category }}</h2>
-
-<ol>
-    @foreach ( $products as $item )
-    <li>{{ $item }}</li>
-    
-    @endforeach
-</ol>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
-use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hey', function (): string {
-    return "halooooo";
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
-Route::prefix('mahasiswa')->group(function (){
-    Route::get('/', [MahasiswaController::class, 'index']);
-    Route::get('/{id}', [MahasiswaController::class, 'detail']);
-});
+require __DIR__.'/auth.php';
